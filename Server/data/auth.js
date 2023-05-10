566d9e90bb1e0d8be54c7095a7bf4963d8fc84d6
Teminam <simplepsm0515@kau.kr>
2023-05-10 16:06:34 +0900

    mysql 적용

--- Server/data/auth.js
+++ Server/data/auth.js
@@ -1,24 +1,15 @@
-let users = [
-    {
-        id:       '1',
-        username: 'melon',
-        password: '$2b$10$21ogSwItePsd8qVpxYa.s.kV5PaOQFGuEWy2yPjWd4wd2U4aJoP8S',
-        name:     '이메론',
-        email:    'melon@melon.com',
-        url:      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS87Gr4eFO7Pt2pE8oym4dxXnxGZYL2Pl_N5A&usqp=CAU'
-    }
-]
+import {db} from '../db/database.js'
 
 export async function findByUsername(username){
-    return users.find((user) => user.username === username)
-}
+    return db.execute('select * from users where username=?', [username]).then((result) => result[0][0])
+} // username 을 전달받아 user의 username과 일치할 시 반환
 
 export async function createUser(user){
-    const created = {...user, id: Date.now().toString()}
-    users.push(created)
-    return created.id
-}
+    const {username, password, name, email, url} = user
+    return db.execute('insert into users (username, password, name, email, url) values (?, ?, ?, ?, ?)', [username, password, name, email, url]).then((result) => result[0].insertId)
+}   // ?에 차례대로 넣어주겠다.
 
 export async function findById(id){
-    return users.find((user) => user.id === id)
+    return db.execute('select id from users where id=?', [id]).then((result) => result[0][0])
+
 }
--- Server/middleware/auth.js
+++ Server/middleware/auth.js
@@ -10,16 +10,19 @@ export const isAuth = async(req, res, next) => {
         return res.status(401).json(AUTH_ERROR)
     }
 
-    const token = authHeader.split(' ')[1]
+    const token = authHeader.split(' ')[1];
+    console.log(token);
     jwt.verify(
         token,                                                                        
         config.jwt.secretKey,
         async(error, decoded) => {
             if(error){
+                console.log("11111");
                 return res.status(401).json(AUTH_ERROR)
             }
             const user = await userRepository.findById(decoded.id)
             if(!user){
+                console.log("22222");
                 return res.status(401).json(AUTH_ERROR)
             }
             req.userId = user.id
